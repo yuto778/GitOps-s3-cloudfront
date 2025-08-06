2d48a91d5b57532f209b7bf89da9da9de6de68ff
yuto778 <yuto4949@i.softbank.jp>
2025-08-07 00:52:38 +0900

    6:fixed

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,11 +1,14 @@
+# GitHubをOIDCプロバイダーとして登録
 resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
+  url            = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
   thumbprint_list = ["2b18947a6a9fc7764fd8b5fb18a863b0c6dac24f"]
 }
 
+# GitHub Actionsが引き受けるためのIAMロール
 resource "aws_iam_role" "github_actions" {
   name = "github-actions-role-for-s3-deploy"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -17,41 +20,54 @@ resource "aws_iam_role" "github_actions" {
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
           StringLike = {
-            "token.actions.githubusercontent.com:sub" : "repo:yuto778/GitOps-s3-cloudfront:*"
+            "token.actions.githubusercontent.com:sub" = "repo:yuto778/GitOps-s3-cloudfront:*"
           }
         }
       }
-
-
     ]
   })
 }
 
+# IAMロールにアタッチするポリシー
 resource "aws_iam_policy" "s3_access" {
-  name = "s3-access-policy-for-gitops"
+  name        = "s3-access-policy-for-gitops"
+  description = "Allows S3 and DynamoDB access for GitOps"
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
+        Sid    = "S3Access"
         Effect = "Allow"
-        Action = [
-          "s3:*"
-        ]
+        Action = "s3:*"
         Resource = [
           "arn:aws:s3:::my-gitops-test-bucket-20250803",
           "arn:aws:s3:::my-gitops-test-bucket-20250803/*",
+          "arn:aws:s3:::terraform-state-215ad062",
+          "arn:aws:s3:::terraform-state-215ad062/*"
         ]
+      },
+      {
+        Sid    = "DynamoDBLockTableAccess"
+        Effect = "Allow"
+        Action = [
+            "dynamodb:GetItem",
+            "dynamodb:PutItem",
+            "dynamodb:DeleteItem"
+        ]
+        Resource = "arn:aws:dynamodb:ap-northeast-1:048588986880:table/terraform-lock-215ad062"
       }
     ]
   })
 }
 
+# ロールとポリシーを紐付け
 resource "aws_iam_role_policy_attachment" "attach_s3_access" {
   role       = aws_iam_role.github_actions.name
   policy_arn = aws_iam_policy.s3_access.arn
 }
 
+# 作成したロールのARNを出力
 output "github_actions_role_arn" {
   value = aws_iam_role.github_actions.arn
 }
