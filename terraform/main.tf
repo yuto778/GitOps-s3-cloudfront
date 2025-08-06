82e4fc15c4bf27750e8dd11a195947a1058e6b63
yuto778 <yuto4949@i.softbank.jp>
2025-08-07 00:26:57 +0900

    4:advance

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,8 @@
 resource "aws_s3_bucket" "website" {
   bucket = "my-gitops-test-bucket-20250803"
 
+  force_destroy = true
+
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
@@ -18,4 +20,33 @@ resource "aws_s3_bucket_website_configuration" "website" {
     key = "error.html"
   }
 
+}
+
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+
+}
+
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  depends_on = [aws_s3_bucket_public_access_block.website]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website.arn}/*"
+      }
+    ]
+  })
 }
